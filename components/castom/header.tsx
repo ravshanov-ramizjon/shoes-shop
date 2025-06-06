7ad4eaa80f68e1b8903e85861ff0db18f5c5b720
ravshanov-ramizjon <ravsanovramiz@gmail.com>
2025-06-06 20:04:18 +0500

    fix mobile version

--- components/castom/header.tsx
+++ components/castom/header.tsx
@@ -152,7 +152,7 @@ export function Header() {
                 )}
 
                 <DropdownMenu>
-                  <DropdownMenuTrigger><MdOutlinePersonOutline className="text-sm text-white hover:text-neon transition " size={20} /></DropdownMenuTrigger>
+                  <DropdownMenuTrigger><MdOutlinePersonOutline className="text-sm text-white hover:text-neon transition " size={20} /> Account</DropdownMenuTrigger>
                   <DropdownMenuContent className="bg-gray-900 text-white border-0">
                     <DropdownMenuLabel>My Account</DropdownMenuLabel>
                     <DropdownMenuSeparator />
